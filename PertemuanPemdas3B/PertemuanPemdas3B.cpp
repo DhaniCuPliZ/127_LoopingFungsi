5f83eea491e631c9d060afcbe4be488e243b7f1b
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2024-03-18 12:25:06 +0700

    Add project files.

---
+++ PertemuanPemdas3B/PertemuanPemdas3B.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int t;
+
+    for (t = 1; t <= 6; t++) 
+    {
+        cout << t << "Dhani" << endl;
+
+    }
+    string arr[5];
+
+    for (int i = 0; i < 5; i++) 
+    {
+        cout << "masukkan nickname : ";
+        cin >> arr[1];
+    }
+
+
+    for (int i = 0; 1 < 5; i++) 
+    {
+        cout << "nilai array: = " << arr[i] << endl;
+
+    }
+    return 0;
+
+}
